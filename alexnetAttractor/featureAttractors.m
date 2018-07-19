89a50c708b01f8a550efa97c302ad86cac8a3b6b
Joel Dapello <joeldapello@localhost.localdomain>
2018-07-19 13:52:06 -0400

    still debugging hopfield??

--- alexnetAttractor/featureAttractors.m
+++ alexnetAttractor/featureAttractors.m
@@ -1,41 +1,37 @@
-function featureAttractors()
-load('data/fc7_features')
+%% load data
+fc7 = load('featureData/fc7FullAndOccPol_325.mat');
+ 
+fc7_full_polarized = fc7.fc7_full_polarized; % polarized fc7 reps of 325 full images
+fc7_occ_polarized = fc7.fc7_occ_polarized;   % polarized fc7 reps of 13k occluded images
 
-%% Train
-% attractor network
-indx = [1,2,5,8,9]; % one fixed point per category
 
-T = double(fc7_train(indx,:)');
+%% create hopfield net
+T = double(fc7_full_polarized');
 net = newhop(T);
 
+%% feed fc7_occ_polarized into hopfield for timesteps and record result in cell 325x1 cell array, each cell has #occluded_imgs x timesteps x features
+% num_objs = length(fc7_occ_polarized);
+num_objs = 325;
+timesteps = 60;
+occ_trajs = cell(num_objs,1);
+for i = 1:num_objs
+    occ_i = fc7_occ_polarized{i};
+    occ_size = size(occ_i);
+    num_occ = occ_size(1);
+    features = occ_size(2);
 
-%% get hopfield trajectories
-features = length(fc7_test(1,:))
-samples = length(fc7_test(:,1))
-timesteps = 60
-
-% for test set of whole images
-fc7_test_hop_trajs = zeros(samples,timesteps,features);
-for i = 1:samples
-    t0 = double(fc7_test(i,:));
-    y = sim(net, {1 timesteps}, {}, {t0'});
-    fc7_test_hop_trajs(i,:,:) = cell2mat(y)';
-    disp(i) 
-    disp(size(t0))
-    disp(size(y))
+    occ_i_trajs = zeros(num_occ,timesteps,features);
+    for j = 1:num_occ
+        t0 = double(occ_i(j,:)');
+        y = sim(net, {1 timesteps}, {}, {t0});
+        occ_i_trajs(j,:,:) = cell2mat(y)';
+    end
+    occ_trajs{i} = occ_i_trajs;
+    disp(i)
 end
 
-% for test set of occluded images
-fc7_test_occ_hop_trajs = zeros(samples,timesteps,features);
-for i = 1:samples
-    t0 = double(fc7_test_occ(i,:));
-    y = sim(net, {1 timesteps}, {}, {t0'});
-    fc7_test_occ_hop_trajs(i,:,:) = cell2mat(y)';
-    disp(i) 
-    disp(size(t0))
-    disp(size(y))
-end
+save('featureData/fc7OccTrajs','occ_trajs','-v7.3')
 
-%% save hopfield trajectories
-% save('fc7_hop_trajs', fc7_test_hop_trajs, fc7_test_occ_hop_trajs)
-save 'fc7_hop_trajs.mat' fc7_test_hop_trajs fc7_test_occ_hop_trajs
+
+%% individual run, most things converging to object 41?
+% i=41; occ_i = fc7_occ_polarized{i}; t0 = double(occ_i(10,:)'); y = sim(net, {1 50}, {}, {t0}); sum(T(:,i)==y{50})
