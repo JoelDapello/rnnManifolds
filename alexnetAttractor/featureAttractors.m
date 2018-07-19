16b0539a0e3cd6c55530157706ba4819de0b82e6
Joel Dapello <joeldapello@localhost.localdomain>
2018-07-19 19:50:18 -0400

    use less data and net directly instead of sim

--- alexnetAttractor/featureAttractors.m
+++ alexnetAttractor/featureAttractors.m
@@ -4,7 +4,6 @@ fc7 = load('featureData/fc7FullAndOccPol_325.mat');
 fc7_full_polarized = fc7.fc7_full_polarized; % polarized fc7 reps of 325 full images
 fc7_occ_polarized = fc7.fc7_occ_polarized;   % polarized fc7 reps of 13k occluded images
 
-
 %% create hopfield net
 T = double(fc7_full_polarized');
 net = newhop(T);
@@ -21,9 +20,9 @@ for i = 1:num_objs
     features = occ_size(2);
 
     occ_i_trajs = zeros(num_occ,timesteps,features);
-    for j = 1:num_occ
+    for j = 1:2:num_occ
         t0 = double(occ_i(j,:)');
-        y = sim(net, {1 timesteps}, {}, {t0});
+        y = net({1 timesteps}, {}, {t0});
         occ_i_trajs(j,:,:) = cell2mat(y)';
     end
     occ_trajs{i} = occ_i_trajs;
@@ -33,5 +32,5 @@ end
 save('featureData/fc7OccTrajs','occ_trajs','-v7.3')
 
 
-%% individual run, most things converging to object 41?
+% individual run, most things converging to object 41?
 % i=41; occ_i = fc7_occ_polarized{i}; t0 = double(occ_i(10,:)'); y = sim(net, {1 50}, {}, {t0}); sum(T(:,i)==y{50})
